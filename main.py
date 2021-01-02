4d299ebbfeb08b7d6b38558b0e01dec8d305e54b
EonZeNx <sparck.va@gmail.com>
2021-01-02 20:19:38 +1300

    Beginning support for args

--- main.py
+++ main.py
@@ -4,11 +4,22 @@
 
 
 # imports
+import argparse as ap
+import os.path
+from typing import List
 
 
 # functions
 def entry():
-	pass
+	parser = ap.ArgumentParser(description=f"Apex Engine Tools v0.3.0")
+	parser.add_argument('process', metavar='path', type=str, nargs='+', help='process each file/folder')
+	
+	args = parser.parse_args()
+	paths: List = []
+	for path in args.process:
+		paths.append(os.path.normpath(path))
+	print(f"Drag and drop WIP")
+	print(f"Path/s dragged: '{', '.join(paths)}'")
 
 
 # entry point
