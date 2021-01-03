70f4af2c987614ed12959ea1a88bc219e68b3f46
EonZeNx <sparck.va@gmail.com>
2021-01-03 22:58:36 +1300

    Basic config support

--- main.py
+++ main.py
@@ -8,17 +8,30 @@ from formats.manager import manage_xml
 import argparse as ap
 import os.path
 from typing import List
+import configparser as cp
 
 
 # settings
 VERSION: str = "0.3.1"
-DB_FILEPATH: str = "./dbs/global.db"
+DB_FILEPATH: str = os.path.abspath("./dbs/global.db")
 SUPPORTED_FILETYPES: List[str] = ["xml", "blo", "epe", "flo"]
 
 
 # functions
-def entry():
+if __name__ == "__main__":
 	""" Entry point for program. """
+	config = cp.ConfigParser()
+	abs_config_path: str = os.path.abspath(".\\config.ini")
+	
+	# TODO: Separate config defaults
+	if not os.path.exists(abs_config_path):
+		config["DEFAULT"] = {}
+		config["DEFAULT"]["DatabaseAbsolutePath"] = DB_FILEPATH
+		with open(abs_config_path, "w") as config_f:
+			config.write(config_f)
+	config.read(abs_config_path)
+	DB_FILEPATH = config["DEFAULT"]["DatabaseAbsolutePath"]
+	
 	parser = ap.ArgumentParser(description=f"Apex Engine Tools {VERSION}")
 	parser.add_argument('process', metavar='path', type=str, nargs='+', help='process each file/folder')
 	
@@ -36,12 +49,7 @@ def entry():
 		else:
 			# process_folder(path)
 			print(f"DEBUG: Folder processing is WIP")
-		print(f"[{i+1}/{len(paths)}] Path/s completed '{path}'")
-
-
-# entry point
-if __name__ == "__main__":
-	entry()
+		print(f"[{i + 1}/{len(paths)}] Path/s completed '{path}'")
 
 
 
