66c1356b96cf584ca50baa87d062977332fe65aa
EonZeNx <sparck.va@gmail.com>
2021-01-03 23:02:20 +1300

    Auto close config settings

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import configparser as cp
 # settings
 VERSION: str = "0.3.1"
 DB_FILEPATH: str = os.path.abspath("./dbs/global.db")
+AUTO_CLOSE: bool = False
 SUPPORTED_FILETYPES: List[str] = ["xml", "blo", "epe", "flo"]
 
 
@@ -27,10 +28,12 @@ if __name__ == "__main__":
 	if not os.path.exists(abs_config_path):
 		config["DEFAULT"] = {}
 		config["DEFAULT"]["DatabaseAbsolutePath"] = DB_FILEPATH
+		config["DEFAULT"]["AutoCloseOnComplete"] = str(int(AUTO_CLOSE))
 		with open(abs_config_path, "w") as config_f:
 			config.write(config_f)
 	config.read(abs_config_path)
 	DB_FILEPATH = config["DEFAULT"]["DatabaseAbsolutePath"]
+	AUTO_CLOSE = bool(int(config["DEFAULT"]["AutoCloseOnComplete"]))
 	
 	parser = ap.ArgumentParser(description=f"Apex Engine Tools {VERSION}")
 	parser.add_argument('process', metavar='path', type=str, nargs='+', help='process each file/folder')
@@ -50,6 +53,9 @@ if __name__ == "__main__":
 			# process_folder(path)
 			print(f"DEBUG: Folder processing is WIP")
 		print(f"[{i + 1}/{len(paths)}] Path/s completed '{path}'")
+	
+	if not AUTO_CLOSE:
+		input("Press any key to continue...")
 
 
 
